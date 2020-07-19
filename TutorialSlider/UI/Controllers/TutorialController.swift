a4a4a968544c0fa9fae059f92d00d34d72030dcd
Oleg Chudnovskiy MB_Pro_13 <oleg.chudnovskiy@gmail.com>
2020-07-19 17:36:28 +0300

    add color to pageControl

--- TutorialSlider/UI/Controllers/TutorialController.swift
+++ TutorialSlider/UI/Controllers/TutorialController.swift
@@ -33,6 +33,10 @@ class TutorialController: UIViewController, UIScrollViewDelegate {
         if let pageControl = pageControl {
             pageControl.numberOfPages = slides.count
             pageControl.currentPage = 0
+            
+            pageControl.pageIndicatorTintColor = UIColor(red: 53/255, green: 132/255, blue: 159/255, alpha: 1)
+            pageControl.currentPageIndicatorTintColor = UIColor(red: 125/255, green: 189/255, blue: 211/255, alpha: 1)
+            
             view.bringSubviewToFront(pageControl)
         }
         scrollView?.contentSize.height = 1.0 // disable vertical scroll
@@ -99,47 +103,5 @@ class TutorialController: UIViewController, UIScrollViewDelegate {
                 customButton?.textToButtonLabel = "Далее"
             }
         }
-        
-    }
-    
-    func scrollView(_ scrollView: UIScrollView, didScrollToPercentageOffset percentageHorizontalOffset: CGFloat) {
-        if let pageControl = pageControl {
-            
-            if(pageControl.currentPage == 0) {
-                //Change background color to toRed: 103/255, fromGreen: 58/255, fromBlue: 183/255, fromAlpha: 1
-                //Change pageControl selected color to toRed: 103/255, toGreen: 58/255, toBlue: 183/255, fromAlpha: 0.2
-                //Change pageControl unselected color to toRed: 255/255, toGreen: 255/255, toBlue: 255/255, fromAlpha: 1
-                
-                let pageUnselectedColor: UIColor = fade(fromRed: 125/255, fromGreen: 189/255, fromBlue: 211/255, fromAlpha: 1, toRed: 103/255, toGreen: 58/255, toBlue: 183/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
-                pageControl.pageIndicatorTintColor = pageUnselectedColor
-                
-                
-                let bgColor: UIColor = fade(fromRed: 103/255, fromGreen: 58/255, fromBlue: 183/255, fromAlpha: 1, toRed: 255/255, toGreen: 255/255, toBlue: 255/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
-                slides[pageControl.currentPage].backgroundColor = bgColor
-                
-                let pageSelectedColor: UIColor = fade(fromRed: 81/255, fromGreen: 36/255, fromBlue: 152/255, fromAlpha: 1, toRed: 103/255, toGreen: 58/255, toBlue: 183/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
-                pageControl.currentPageIndicatorTintColor = pageSelectedColor
-            }
-        }
-    }
-    
-    
-    func fade(fromRed: CGFloat,
-              fromGreen: CGFloat,
-              fromBlue: CGFloat,
-              fromAlpha: CGFloat,
-              toRed: CGFloat,
-              toGreen: CGFloat,
-              toBlue: CGFloat,
-              toAlpha: CGFloat,
-              withPercentage percentage: CGFloat) -> UIColor {
-        
-        let red: CGFloat = (toRed - fromRed) * percentage + fromRed
-        let green: CGFloat = (toGreen - fromGreen) * percentage + fromGreen
-        let blue: CGFloat = (toBlue - fromBlue) * percentage + fromBlue
-        let alpha: CGFloat = (toAlpha - fromAlpha) * percentage + fromAlpha
-        
-        // return the fade colour
-        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
