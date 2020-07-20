d6a2e913807d92d93fceca5f4d8459aae5a551bf
Oleg Chudnovskiy MB_Pro_13 <oleg.chudnovskiy@gmail.com>
2020-07-20 11:48:24 +0300

    add SlideModel class

---
+++ TutorialSlider/Model/SlideModel.swift
@@ -0,0 +1,15 @@
+//
+//  SlideModel.swift
+//  TutorialSlider
+//
+//  Created by OlegChudnovskiy on 20.07.2020.
+//  Copyright © 2020 OlegChudnovskiy. All rights reserved.
+//
+
+import Foundation
+
+struct SlideModel {
+    private var title: String
+    private var image: String
+    private var icons: String
+}
